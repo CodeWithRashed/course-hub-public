adbceb14a7b36358b59b937ea120b10333898aab
Md Rashed Ali <tools.rashed@gmail.com>
2023-09-14 22:14:32 +0600

    Course Registration Text Add

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <>
       <div className="app-container px-12 py-12 bg-[#F3F3F3]">
-        <div className="title">Course Registration</div>
+        <div className="title text-[#1C1B1B] text-3xl font-bold mb-8 text-center">
+          Course Registration
+        </div>
         <Homepage></Homepage>
       </div>
     </>
